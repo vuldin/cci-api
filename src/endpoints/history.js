b42a09028f56e3ad8357a0c94d3ac391b32cda4a
Joshua Purcell <jlpurcell@us.ibm.com>
2017-06-01 09:12:51 -0700

    connected to cci, history and profile working

--- src/endpoints/history.js
+++ src/endpoints/history.js
@@ -1,10 +1,40 @@
-import data from '../data/history.json'
+import request from 'superagent'
+import dotenv from 'dotenv'
 
-export default function(server) {
-  server.get( '/history', (req, res) => {
+export default async function (server) {
+  let data = []
+  dotenv.config()
+  const agent = request.agent()
+  await agent
+    .get('http://172.27.12.35:8009/cci/api/v1/login')
+    .query({
+      user: process.env.CCI_USER,
+      password: process.env.CCI_PASSWORD
+    })
+    .then(async function () {
+      try {
+        data = await agent
+          .get('http://172.27.12.35:8009/cci/api/v1/subscriber/histdata')
+          .query({
+            aggr: 'd',
+            imsi: '272211221122000',
+            timefrom: '20170601000000',
+            timeto: '20170620000000'
+          })
+        data = JSON.parse(data.text)
+      } catch (e) {
+        console.log('error')
+        console.log(e)
+      }
+    })
+  server.get('/history', (req, res) => {
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','X-Requested-With')
     res.send(data)
   })
-  server.post( '/history', (req, res) => {
+  server.post('/history', (req, res) => {
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','X-Requested-With')
     res.send(data)
   })
 }
--- src/endpoints/profile.js
+++ src/endpoints/profile.js
@@ -1,10 +1,14 @@
-import data from '../data/profile.json'
+import data from '../../data/profile.json'
 
 export default function(server) {
   server.get( '/profile', (req, res) => {
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','X-Requested-With')
     res.send(data)
   })
   server.post( '/profile', (req, res) => {
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','X-Requested-With')
     res.send(data)
   })
 }
